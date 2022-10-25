0b63a58c5f1e493f19409859fc91e021f8179b66
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-10-25 21:33:38 +0300

    Update User.java

--- MiniORM/src/main/java/entities/User.java
+++ MiniORM/src/main/java/entities/User.java
@@ -1,14 +1,20 @@
 package entities;
 
+import orm.annotations.Column;
+import orm.annotations.Entity;
+import orm.annotations.Id;
+
 import java.time.LocalDate;
 
+@Entity(name = "users")
 public class User {
+    @Id(name = "id")
     private long id;
-
+@Column(name="user_name")
     private String username;
-
+@Column(name ="age")
     private int age;
-
+@Column(name="registration_date")
     private LocalDate registration;
 
     public User(String username, int age, LocalDate registration) {
