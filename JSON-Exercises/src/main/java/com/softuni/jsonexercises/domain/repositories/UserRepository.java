fa6cf40e7d4c0224bdb4a74dc99dd410129bc23b
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-11-19 18:23:17 +0200

    Update UserRepository.java

--- JSON-Exercises/src/main/java/com/softuni/jsonexercises/domain/repositories/UserRepository.java
+++ JSON-Exercises/src/main/java/com/softuni/jsonexercises/domain/repositories/UserRepository.java
@@ -4,8 +4,13 @@ import com.softuni.jsonexercises.domain.entities.Category;
 import com.softuni.jsonexercises.domain.entities.Product;
 import com.softuni.jsonexercises.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+    @Query(value = "select * from `json`.users order by RAND() limit 1", nativeQuery = true)
+    Optional<User> getRandomUser();
 }
