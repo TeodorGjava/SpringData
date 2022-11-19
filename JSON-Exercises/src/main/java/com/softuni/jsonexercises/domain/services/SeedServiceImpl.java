0462b3f7f0f59ef100165dd9eecb094e3d9929d8
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-11-19 18:23:22 +0200

    Update SeedServiceImpl.java

--- JSON-Exercises/src/main/java/com/softuni/jsonexercises/domain/services/SeedServiceImpl.java
+++ JSON-Exercises/src/main/java/com/softuni/jsonexercises/domain/services/SeedServiceImpl.java
@@ -1,19 +1,26 @@
 package com.softuni.jsonexercises.domain.services;
 
+import com.softuni.jsonexercises.domain.entities.Category;
+import com.softuni.jsonexercises.domain.entities.Product;
 import com.softuni.jsonexercises.domain.entities.User;
-import com.softuni.jsonexercises.domain.entities.dtos.ImportUserDTO;
+import com.softuni.jsonexercises.domain.entities.dtos.categories.CategoryImportDTO;
+import com.softuni.jsonexercises.domain.entities.dtos.products.ProductImportDTO;
+import com.softuni.jsonexercises.domain.entities.dtos.users.ImportUserDTO;
 import com.softuni.jsonexercises.domain.repositories.CategoryRepository;
 import com.softuni.jsonexercises.domain.repositories.ProductRepository;
 import com.softuni.jsonexercises.domain.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Arrays;
-import java.util.List;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.stream.IntStream;
 
-import static com.softuni.jsonexercises.constants.Paths.JSON_USERS_PATH;
+import static com.softuni.jsonexercises.constants.Paths.*;
 import static com.softuni.jsonexercises.constants.Utils.GSON;
 import static com.softuni.jsonexercises.constants.Utils.MODEL_MAPPER;
 
@@ -36,16 +43,70 @@ public class SeedServiceImpl implements SeedService {
             final FileReader fileReader = new FileReader(JSON_USERS_PATH.toFile());
             List<User> users = Arrays.stream(GSON.fromJson(fileReader, ImportUserDTO[].class))
                     .map(userDTo -> MODEL_MAPPER.map(userDTo, User.class)).toList();
-            this.userRepository.saveAll(users);
+            this.userRepository.saveAllAndFlush(users);
         }
     }
 
     @Override
-    public void seedCategories() {
+    public void seedCategories() throws IOException {
+        if (this.categoryRepository.count() == 0) {
+            final FileReader fileReader = new FileReader(JSON_CATEGORIES_PATH.toFile());
+            final List<Category> categories = Arrays.stream(GSON.fromJson(fileReader, CategoryImportDTO[].class))
+                    .map(categoryImportDTO -> MODEL_MAPPER.map(categoryImportDTO, Category.class)).toList();
+            this.categoryRepository.saveAllAndFlush(categories);
+            fileReader.close();
+        }
     }
 
     @Override
-    public void seedProducts() {
+    public void seedProducts() throws IOException {
+        if (this.productRepository.count() == 0) {
+            final FileReader fileReader = new FileReader(JSON_PRODUCTS_PATH.toFile());
+            List<Product> products = Arrays.stream(GSON.fromJson(fileReader, ProductImportDTO[].class))
+                    .map(productImportDTO -> MODEL_MAPPER.map(productImportDTO, Product.class))
+                    .map(this::setRandomSeller)
+                    .map(this::setRandomBuyer)
+                    .map(this::setRandomCategory)
+                    .toList();
+            this.productRepository.saveAllAndFlush(products);
+            fileReader.close();
+        }
+    }
+
+    private Product setRandomCategory(Product product) {
+        Category category = this.categoryRepository.getRandomCategory().orElseThrow(NoSuchElementException::new);
+        Random random = new Random();
+        Long highEnd = this.categoryRepository.count();
+        int numberOfCategories = random.nextInt(highEnd.intValue());
+        Set<Category> categories = new HashSet<>();
+
+        IntStream.rangeClosed(1, numberOfCategories)
+                .forEach(num -> {
+                    this.categoryRepository.getRandomCategory()
+                            .orElseThrow(NoSuchElementException::new);
+                    categories.add(category);
+                });
+
+        product.setCategories(categories);
+        return product;
+    }
+
+    private Product setRandomBuyer(Product product) {
+        if (product.getPrice().compareTo(new BigDecimal(800L)) > 0) {
+            User buyer = this.userRepository.getRandomUser().orElseThrow(NoSuchElementException::new);
+            while (!buyer.equals(product.getSeller())) {
+                buyer = this.userRepository.getRandomUser().orElseThrow(NoSuchElementException::new);
+            }
+            product.setBuyer(buyer);
+
+        }
+        return product;
+    }
+
+    private Product setRandomSeller(Product product) {
+        final User user = this.userRepository.getRandomUser().orElseThrow(NoSuchElementException::new);
+        product.setSeller(user);
+        return product;
     }
 
 }
