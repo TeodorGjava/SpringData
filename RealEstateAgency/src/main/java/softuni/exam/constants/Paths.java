c004d7f752324806ac08a4f7e90167064601ec0a
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-12-01 19:04:00 +0200

    paths

---
+++ RealEstateAgency/src/main/java/softuni/exam/constants/Paths.java
@@ -0,0 +1,11 @@
+package softuni.exam.constants;
+
+import java.nio.file.Path;
+
+public enum Paths {
+    ;
+    public static final Path PATH_OF_AGENTS_JSON = Path.of("src/main/resources/files/json/agents.json");
+    public static final Path PATH_OF_TOWNS_JSON = Path.of("src/main/resources/files/json/towns.json");
+    public static final Path PATH_OF_APARTMENTS_XML = Path.of("src/main/resources/files/xml/apartments.xml");
+    public static final Path PATH_OF_OFFERS_XML = Path.of("src/main/resources/files/xml/offers.xml");
+}
